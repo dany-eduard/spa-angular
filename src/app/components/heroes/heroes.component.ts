4d753e58633e73fdc2b9e340d2f83ee1afef54b6
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-07-22 00:45:00 -0500

    Se crea el componente

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    //console.log(this.heroes);
+  }
+
+  verHeroe(i: number) {
+    this.router.navigate(['heroes/heroe', i]);
+  }
+}
