9b607aaeef2f0a068c80eed4ebaff4c84c4a79e6
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-07-26 00:02:04 -0500

    Se muestra el perfil de los heroes en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    /* console.log(this.heroes); */
   }
-
 }
