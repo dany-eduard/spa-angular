5f7c068de157fd2c9a575d4c2b1974732650db95
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-07-26 00:05:27 -0500

    Rdirección al componente result, para mostrar el resultado de las busquedas

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  buscarHeroe(termino: string) {
+    if (termino === '') {
+      return;
+    }
+    this.router.navigate(['heroes/resultado', termino]);
   }
-
 }
